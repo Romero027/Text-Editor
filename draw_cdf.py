5f21613e4488094701b350deb43c928cff27b0d8
Xiangfeng(Allen) Zhu <xzhu27@ucsc.edu>
2020-02-10 23:00:03 -0500

    update

--- draw_cdf.py
+++ draw_cdf.py
@@ -2,14 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import *
 import re, math
-import seaborn as sns
+#import seaborn as sns
 from matplotlib import rcParams
 import pandas as pd
 import matplotlib, csv
 from matplotlib import rc
 #from pyemd import emd
-import dit
-from dit.divergences import jensen_shannon_divergence
+#import dit
+#from dit.divergences import jensen_shannon_divergence
 import pickle
 
 rc('font',**{'family':'serif','serif':['Times']})
@@ -84,14 +84,12 @@ def plot_cdf(datas, linelabels = None, label = None, y_label = "CDF", name = "ss
 
     #plt.xlim(0, 1.21e6)#, 150)
     #plt.xlim(0, 1.0)
-    plt.xlim(0, 2000)
+    #plt.xlim(0, 2000)
     #plt.show()
 
     plt.savefig(name)
 
-filename = "sampleSizes.txt"
-infile = open(filename, 'rb')
-sumOfSamples = pickle.load(infile)
+sumOfSamples = [3984, 9494, 10567, 119775, 30, 139630, 846, 7458, 119235, 2827, 15654, 7764, 69, 14148, 334049, 20900, 17541, 112457, 44601, 56189, 6501, 1223, 112900, 600, 34485, 22314, 129705, 8043, 66634, 5, 162, 28211, 52653, 83819, 9987, 385, 80555, 477, 7006, 10855, 11311, 13686, 87199, 11455, 12777, 1357, 19791, 138493, 3634, 37667, 13822, 1574, 37974, 66294, 329, 5590, 14095, 14495, 64045, 4000, 61153, 11058, 28735, 311562, 7831, 1262, 33254, 45245, 13900, 5714, 29812, 37868, 69, 15118, 1082, 97, 163225, 8698, 61327, 55033, 17444, 678008, 245418, 80, 2113, 2364, 535410, 168260, 27475, 12887, 1870, 7866, 28231, 4651, 162355, 4667, 30228, 2169, 1230, 9044, 488395, 45387, 313394, 18431, 265536, 2418, 96739, 963, 11139, 956, 10463, 3468, 17810, 21779, 1083, 3268, 140525, 22490, 46332, 109924, 7208, 126088, 24129, 20751, 127823, 1597, 17602, 1221, 16221, 16074, 78117, 46854, 69, 1422, 46973, 8272, 42355, 3197, 77, 39499, 12821, 1883, 93385, 942, 2788, 68582, 438828, 53330, 138654, 65650]
 
 plot_cdf([sumOfSamples], [''], "\# of Samples per Client", "CDF", "sizeImg.pdf")
 #plot_cdf([divBigger], [''], "Data Divergence across Countries", "CDF", "cDiv.pdf")
